0c3402ad100469f642fdf6d7ba83597d955145d2
Panagiotis Georgiadis <pgeorgia@redhat.com>
2020-10-12 03:48:10 +0200

    Second

---
+++ internal/astrobot/git.go
@@ -0,0 +1,137 @@
+package astrobot
+
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/drpaneas/dudenetes/pkg/run"
+)
+
+// ChangeBranch changes to another branch
+func ChangeBranch(branch string) {
+	cmd := fmt.Sprintf("git checkout -b %s", branch)
+	log.Println("Running: ", cmd)
+	timeout := 5
+	directory := "/Users/drpaneas/github/starlordgr"
+	output, err := run.SlowCmdDir(cmd, timeout, directory)
+	if err != nil {
+		CheckoutMaster()
+		log.Fatal(err)
+	}
+	log.Println(output)
+	output, _ = run.SlowCmdDir("git branch --show-current", timeout, directory)
+	log.Println(output)
+}
+
+// IsItUpToDate checks if the repo is clear
+func IsItUpToDate() (answer bool) {
+	log.Println("Checking if repo is up to date ...")
+	answer = false
+	cmd := "git pull origin master"
+	timeout := 5
+	directory := "/Users/drpaneas/github/starlordgr"
+	output, err := run.SlowCmdDir(cmd, timeout, directory)
+	if err != nil {
+		CheckoutMaster()
+		log.Fatal(err)
+	}
+	if strings.Contains(output, "Already up to date.") {
+		answer = true
+	}
+	return answer
+}
+
+// GitAdd runs git add
+func GitAdd() {
+	log.Println("Running git add .")
+	cmd := "git add ."
+	timeout := 5
+	directory := "/Users/drpaneas/github/starlordgr"
+	output, err := run.SlowCmdDir(cmd, timeout, directory)
+	if err != nil {
+		branch, _ := run.SlowCmdDir("git --no-pager log -1", timeout, directory)
+		revertCleanState(branch)
+		log.Fatal(err)
+	}
+	log.Println(output)
+	output, _ = run.SlowCmdDir("git status", timeout, directory)
+	log.Println(output)
+}
+
+// GitCommit commits the stuff
+func GitCommit() {
+	cmd := "git commit -a --allow-empty-message -m ''"
+	log.Println("Running: ", cmd)
+	timeout := 5
+	directory := "/Users/drpaneas/github/starlordgr"
+	output, err := run.SlowCmdDir(cmd, timeout, directory)
+	if err != nil {
+		branch, _ := run.SlowCmdDir("git --no-pager log -1", timeout, directory)
+		revertCleanState(branch)
+		log.Fatal(err)
+	}
+	log.Println(output)
+	output, _ = run.SlowCmdDir("git --no-pager log -1", timeout, directory)
+	log.Println(output)
+}
+
+// GitPush on a given branch
+func GitPush(branch string) {
+	cmd := fmt.Sprintf("git push origin %s", branch)
+	log.Println("Running: ", cmd)
+	timeout := 10
+	directory := "/Users/drpaneas/github/starlordgr"
+	output, err := run.SlowCmdDir(cmd, timeout, directory)
+	if err != nil {
+		branch, _ := run.SlowCmdDir("git --no-pager log -1", timeout, directory)
+		revertCleanState(branch)
+		log.Fatal(err)
+	}
+	log.Println(output)
+}
+
+// CheckoutMaster checks out at master
+func CheckoutMaster() {
+	log.Println("Checking out at master branch")
+	cmd := "git checkout master"
+	timeout := 5
+	directory := "/Users/drpaneas/github/starlordgr"
+	output, err := run.SlowCmdDir(cmd, timeout, directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(output)
+}
+
+// DeleteBranch deletes a branch with force
+func DeleteBranch(branch string) {
+	cmd := fmt.Sprintf("git branch -D %s", branch)
+	log.Println("Running: ", cmd)
+	timeout := 5
+	directory := "/Users/drpaneas/github/starlordgr"
+	output, err := run.SlowCmdDir(cmd, timeout, directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(output)
+}
+
+// gitResetHardHead deletes any changes to be commited
+func gitResetHardHead() {
+	cmd := "git reset --hard HEAD"
+	log.Println("Running: ", cmd)
+	timeout := 5
+	directory := "/Users/drpaneas/github/starlordgr"
+	output, err := run.SlowCmdDir(cmd, timeout, directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(output)
+}
+
+func revertCleanState(branch string) {
+	CheckoutMaster()
+	gitResetHardHead()
+	DeleteBranch(branch)
+}
---
+++ pkg/unboxholics/unboxholics.go
@@ -0,0 +1,90 @@
+package unboxholics
+
+import (
+	"log"
+	"regexp"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/hashicorp/go-retryablehttp"
+)
+
+const (
+	url string = "https://unboxholics.com/tag/space"
+)
+
+// Doc for EarthSky
+var Doc *goquery.Document = getHTML(url)
+
+// NewsDBUnboxholics db with the news
+var NewsDBUnboxholics []News
+
+// News represent an news article
+type News struct {
+	Title       string `json:"title"`
+	Link        string `json:"link"`
+	Description string `json:"description"`
+	Image       string `json:"image"`
+	GreekTitle  string `json:"greektitle"`
+	GreekDesc   string `json:"greekdesc"`
+	Source      string `json:"source"`
+}
+
+// GetNews fetches the news of Unboxholics.com
+func GetNews() {
+	var title string
+	var image string
+	var desc string
+	var link string
+	var ok bool
+
+	Doc.Find("#main-container > div > div > article").Each(func(i int, s *goquery.Selection) {
+		s.Find("div.entry__body.post-list__body.card__body > div.entry__header > h2 > a").Each(func(i int, s *goquery.Selection) {
+			link, ok = s.Attr("href")
+			if ok {
+				title = s.Text()
+			}
+		})
+		s.Find("div.entry__img-holder.post-list__img-holder.card__img-holder > img").Each(func(i int, s *goquery.Selection) {
+			img, ok := s.Attr("data-src")
+			if ok {
+				image = img
+			}
+		})
+		s.Find("div.entry__body.post-list__body.card__body > div.entry__excerpt > p").Each(func(i int, s *goquery.Selection) {
+			desc = s.Text()
+			// Remove newlines
+			re := regexp.MustCompile(`\r?\n`)
+			desc = re.ReplaceAllString(desc, " ")
+		})
+		NewsDBUnboxholics = append(NewsDBUnboxholics, News{
+			Description: desc,
+			Image:       image,
+			Link:        link,
+			Title:       title,
+			Source:      "unboxholics.com",
+		})
+	})
+}
+
+func getHTML(page string) (doc *goquery.Document) {
+	retryClient := retryablehttp.NewClient()
+	retryClient.RetryMax = 10
+	standardClient := retryClient.StandardClient() // *http.Client
+
+	// Request the HTML page.
+	res, err := standardClient.Get(page)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err = goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return doc
+}
