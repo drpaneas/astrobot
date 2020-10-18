679ac979280e34bfd328967d07888fa0498736fc
Panagiotis Georgiadis <pgeorgia@redhat.com>
2020-10-18 12:17:54 +0200

    Add Skai

--- pkg/skai/skai.go
+++ pkg/skai/skai.go
@@ -12,8 +12,9 @@ import (
 )
 
 const (
-	url     string = "https://www.skai.gr/tags/astronomia"
-	baseURL string = "https://www.skai.gr"
+	url       string = "https://www.skai.gr/tags/astronomia"
+	baseURL   string = "https://www.skai.gr"
+	urlSpaceX string = "https://www.skai.gr/tags/space-x"
 )
 
 func stripSpaces(s string) string {
@@ -24,6 +25,7 @@ func stripSpaces(s string) string {
 
 // Doc for skai.gr
 var Doc *goquery.Document = getHTML(url)
+var docSpaceX *goquery.Document = getHTML(urlSpaceX)
 
 // NewsDBskai db with the news
 var NewsDBskai []News
@@ -76,6 +78,47 @@ func GetNews() {
 	doc := getHTML(link)
 	descQuery := fmt.Sprintf("body > div.dialog-off-canvas-main-canvas > div.jscroll2.jscroll > div > main > div > div.viewWithSideBar > article > div.mainInfo > p")
 	doc.Find(descQuery).Each(func(i int, s *goquery.Selection) {
+		desc = s.Text()
+		// Remove newlines
+		re := regexp.MustCompile(`\r?\n`)
+		desc = re.ReplaceAllString(desc, " ")
+		if desc[0:1] == " " {
+			desc = strings.TrimSpace(desc)
+		}
+	})
+	NewsDBskai = append(NewsDBskai, News{
+		Description: desc,
+		Image:       image,
+		Link:        link,
+		Source:      "skai.gr",
+		Title:       title,
+	})
+
+	docSpaceX.Find("body > div.dialog-off-canvas-main-canvas > main > div:nth-child(1) > div.categoryPinned.grid-x.grid-margin-x.medium-up-2.large-up-4 > div:nth-child(1) > div > div.cmnArticleTitlePad > a").Each(func(i int, s *goquery.Selection) {
+		link, ok = s.Attr("href")
+		if ok {
+			link = baseURL + link
+		}
+		title = s.Text()
+		if strings.Contains(title, "Space X") {
+			title = strings.Split(title, "Space X")[1]
+		}
+		re := regexp.MustCompile(`\r?\n`)
+		title = re.ReplaceAllString(title, " ")
+		if string(title[0]) == " " {
+			title = strings.TrimSpace(title)
+		}
+	})
+
+	imageQuery = fmt.Sprintf("body > div.dialog-off-canvas-main-canvas > main > div:nth-child(1) > div.categoryPinned.grid-x.grid-margin-x.medium-up-2.large-up-4 > div:nth-child(1) > div > div.imgAligner > div > img")
+	docSpaceX.Find(imageQuery).Each(func(i int, s *goquery.Selection) {
+		tmpimage, _ := s.Attr("src")
+		tmp := strings.Split(tmpimage, "?")
+		image = baseURL + tmp[0]
+	})
+	docSpaceXdoc := getHTML(link)
+	descQuery = fmt.Sprintf("body > div.dialog-off-canvas-main-canvas > div.jscroll2.jscroll > div > main > div > div.viewWithSideBar > article > div.mainInfo > p")
+	docSpaceXdoc.Find(descQuery).Each(func(i int, s *goquery.Selection) {
 		desc = s.Text()
 		// Remove newlines
 		re := regexp.MustCompile(`\r?\n`)
